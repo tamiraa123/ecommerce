95c609fab9c29c7f3435613ad98b0fd83fce981b
Munkhzorig <emuuguu1220@gmail.com>
2020-07-29 18:22:13 -0500

    product list for vendor

--- src/routes.js
+++ src/routes.js
@@ -15,7 +15,7 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-import ProductList from "views/Vendor/ProductList";
+import vProducts from "views/Vendor/Products.jsx";
 import Dashboard from "views/Dashboard.jsx";
 import UserProfile from "views/UserProfile.jsx";
 import TableList from "views/TableList.jsx";
@@ -43,7 +43,7 @@ const dashboardRoutes = [
     path: "/myProducts",
     name: "My Products",
     icon: "pe-7s-keypad",
-    component: ProductList,
+    component: vProducts,
     layout: "/admin"
   },
   {
