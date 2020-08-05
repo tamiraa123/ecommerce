7220c14675989d9c8953096f88c82925117bbdb5
tamir <tamir.baldandorj@gmail.com>
2020-08-04 20:34:13 -0500

    asdfas

--- src/views/Admin/Test.jsx
+++ src/views/Admin/Test.jsx
@@ -41,7 +41,6 @@ class Test extends Component {
         })    
   }
   handleSave = ()=>{
-
       let bucketName = 'images'
       let file = this.state.files[0]
       let storageRef = firebase.storage().ref(`${bucketName}/${file.name}`)
